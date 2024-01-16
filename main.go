5703651165f9ad9a5220a9dc21f3aab9a9df9bdd
Honk The Berry <honktheberry@gmail.com>
2024-01-15 20:11:27 -0500

    Program now offers a help menu to users who enter the program name without specifying options, and ends the program :DDD

--- main.go
+++ main.go
@@ -2,14 +2,23 @@ package main
 
 import (
 	"flag"
-	// "fmt"
+	"fmt"
+	"os"
+
 	// "strings"
 
 	"github.com/kkdai/youtube/v2"
 	// "github.com/kkdai/youtube/v2"
 )
 
+const (
+	Outfiletext = "-o              Output filepath. Do not specify a file extension. Default is $HOME/something.mp4"
+	URLText     = "-u              URL of video to download"
+	TypeText    = "-t              Accepts 'audio' or 'video' as input."
+)
+
 func main() {
+
 	//Define flags
 	videoOutfile := flag.String("o", "", "Output filepath. Do not specify a file extension. Default is $HOME/something.mp4")
 	videoURL := flag.String("u", "https://www.youtube.com/watch?v=dQw4w9WgXcQ", "URL of video to download")
@@ -17,6 +26,12 @@ func main() {
 	// help := flag.String("h", "", "Outputs a help dialog, showing how to use the program.")
 
 	flag.Parse()
+	if flag.NFlag() == 0 {
+		fmt.Println("=========================HELP=========================\n")
+		fmt.Println(Outfiletext + "\n\n" + URLText + "\n\n" + TypeText + "\n")
+		fmt.Println("=========================HELP=========================")
+		os.Exit(1234)
+	}
 
 	*videoOutfile = getDefaults(*videoOutfile)
 	client := youtube.Client{}
